b83e93714b585d5b0c68f6c0e3d2a331fc493d02
Lejo1 <Lejo_1@web.de>
2018-11-14 17:30:15 +0100

    Update antispam

--- mods/antispam/init.lua
+++ mods/antispam/init.lua
@@ -1,11 +1,11 @@
 local PLAYERS_MSG={}
 local PLAYERS_FREQ={}
 local SPAM_SPEED=5
-local SPAM_SPEED_MSECS=SPAM_SPEED*1000
+local SPAM_SPEED_MSECS=SPAM_SPEED*1000000
 local SPAM_WARN=5
 local SPAM_KICK=SPAM_WARN+5
 local RESET_TIME=30 --In seconds
-local RESET_TIME_MSECS=RESET_TIME*1000 --Convert to microsecs
+local RESET_TIME_MSECS=RESET_TIME*1000000 --Convert to microsecs
 local WARNING_COLOR=minetest.get_color_escape_sequence("#FFBB33")
 minetest.register_on_leaveplayer(function (player)
     PLAYERS_MSG[player:get_player_name()]=nil
@@ -17,23 +17,22 @@ minetest.register_on_joinplayer(function (player)
     end,player)
 end)
 minetest.register_on_chat_message(function(name,message)
-    if minetest.get_player_privs(name).kick then
-      return
-    end
     for msg,info in pairs(PLAYERS_MSG[name]) do
         if minetest.get_us_time()-info[2] >= RESET_TIME_MSECS then
             PLAYERS_MSG[name][msg]=nil
         end
     end
+    local toreturn
     if PLAYERS_MSG[name][message] then
         local amount=PLAYERS_MSG[name][message][1]+1
         PLAYERS_MSG[name][message][1]=amount
         PLAYERS_MSG[name][message][2]=minetest.get_us_time()
         if amount >= SPAM_KICK then
             minetest.kick_player(name,"Kicked for spamming.")
+            toreturn = true
         elseif amount >= SPAM_WARN then
             minetest.chat_send_player(name,WARNING_COLOR.."Warning ! You've sent the message '"..message.."' too often. Wait at least "..RESET_TIME.." seconds before sending it again.")
-            return true
+            toreturn = true
         end
     else
         PLAYERS_MSG[name][message]={1,minetest.get_us_time()}
@@ -43,17 +42,17 @@ minetest.register_on_chat_message(function(name,message)
         local amount=PLAYERS_FREQ[name][2]
         local speed=PLAYERS_FREQ[name][1]
         local delay=minetest.get_us_time()-PLAYERS_FREQ[name][3]
-        speed=speed*amount+delay/(amount+1)
+        speed=(speed*amount+delay)/(amount+1)
         if amount >= SPAM_WARN then
-            if warns+1>=SPAM_KICK-SPAM_WARN then
+            if warns+1==SPAM_KICK-SPAM_WARN then
                 minetest.kick_player(name,"Kicked for spamming.")
-            elseif speed >= SPAM_SPEED_MSECS then
+                toreturn = true
+            elseif speed <= SPAM_SPEED_MSECS then
                 minetest.chat_send_player(name,WARNING_COLOR.."Warning ! You are sending messages too fast. Wait at least "..SPAM_SPEED.." seconds before sending another message.")
                 warns=warns+1
-                speed=SPAM_SPEED
+                speed=SPAM_SPEED_MSECS
                 amount=SPAM_WARN
-                PLAYERS_FREQ[name]={speed,amount+1,minetest.get_us_time(),warns}
-                return true
+                toreturn = true
             else
                 speed=0
                 amount=0
@@ -64,4 +63,5 @@ minetest.register_on_chat_message(function(name,message)
     else
         PLAYERS_FREQ[name]={0,0,minetest.get_us_time(),0}
     end
+    return toreturn
 end)
