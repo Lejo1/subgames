071760880f2546c1fc34ad189e925e1301e66109
Lejo1 <Lejo_1@web.de>
2018-12-02 17:03:40 +0100

    Fix antikick in hide and seek

--- mods/afkkick/init.lua
+++ mods/afkkick/init.lua
@@ -43,7 +43,7 @@ minetest.register_globalstep(function(dtime)
 	for playerName,_ in pairs(players) do
 		local player = minetest.get_player_by_name(playerName)
 		if player then
-		if hiddenseeker.disguis[name] and hiddenseeker.disguis[name].enable then return end
+		if hiddenseeker.disguis[playerName] and hiddenseeker.disguis[playerName].enable then return end
 			--Check for inactivity once every CHECK_INTERVAL seconds
 			checkTimer = checkTimer + dtime
 			if checkTimer > CHECK_INTERVAL then
